69288d4b14062fd1cd186dfaaf5f712b130ac83f
Daniel J.Mazure <daniel.jimenez2@bsc.es>
2022-12-05 00:22:42 +0100

    Make mem parameters generic

--- src/axi_brom_top.sv
+++ src/axi_brom_top.sv
@@ -17,24 +17,25 @@
 // Description: 
 
 
-module axi_brom (
-         
- input  [15:0] addra ,
- input  clka         ,
- input  [127:0] dina ,
- output [127:0] douta,
- input  ena          ,
- input  [15:0] wea   
+module #(
+  // channel for BRAM and DRAM
+  parameter integer MEM_DATA_WIDTH = 128;
+  parameter integer BRAM_ADDR_WIDTH = 16;     // 64 KB
+)
+axi_brom (          
+  input  [BRAM_ADDR_WIDTH-1:0] addra,
+  input  clka,
+  input  [MEM_DATA_WIDTH-1:0] dina,
+  output [MEM_DATA_WIDTH-1:0] douta,
+  input  ena,
+  input  [(MEM_DATA_WIDTH/8)-1:0] wea   
 );
 
 
 
 // channel for BRAM and DRAM
-localparam MEM_DATA_WIDTH = 128;
-localparam BRAM_ADDR_WIDTH = 16;     // 64 KB
 localparam BRAM_LINE = 2 ** BRAM_ADDR_WIDTH  * 8 / MEM_DATA_WIDTH;
 localparam BRAM_LINE_OFFSET = $clog2(MEM_DATA_WIDTH/8); 
-  
  
  // the inferred BRAMs
  reg [MEM_DATA_WIDTH-1:0] ram [0 : BRAM_LINE-1];
